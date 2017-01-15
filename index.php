abf245b768a88bb5074dde6d70c5d27e847557ab
bl0451 <bl0451@wayne.edu>
2017-01-15 15:44:38 -0500

    Initial html-only model

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>TODO List Selection</title>
+    <meta http-equiv="content-type"
+    content="text/html; charset=utf-8"/>
+  </head>
+  <body>
+    <p>Please enter a user or list name to load</p>
+<!-    <form action="select_list.php">
+    <form>
+    List name: <input type="text" name="listname"><br>
+    <input type="submit" value="Submit">
+    </form>
+  </body>
+</html>
